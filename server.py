ff5f1c9ca6ad9721b15c6a044c464d972f7a2420
Ghaith Al-Maliki <122114790+GhaithA-M@users.noreply.github.com>
2024-12-08 21:25:49 +0100

    Implemented flask

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')  # Serve index.html from the root directory
+    return send_from_directory('', 'index.html')
 
 @app.route('/data/communes/<filename>')
 def commune_data(filename):
-    return send_from_directory('data/communes', filename)  # Serve data from the data/communes folder
+    return send_from_directory('data/communes', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=80)
